88061b292d7932b2239d4b9938a6a35e013b4a03
= <=>
2024-07-18 11:26:49 +0530

    not working, just a backup

--- device.mk
+++ device.mk
@@ -4,4 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+
 LOCAL_PATH := device/samsung/m02s
+
+
+#PRODUCT_PACKAGES += \
+    android.hardware.input.classifier@1.0-service \
--- omni_m02s.mk
+++ omni_m02s.mk
@@ -7,6 +7,10 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit from m02s device
 $(call inherit-product, device/samsung/m02s/device.mk)
 
+#Other inherits
+#$(call inherit-product, vendor/twrp/config/packages.mk)
+$(call inherit-product, $(LOCAL_PATH)/libopenaes.mk)
+
 PRODUCT_DEVICE := m02s
 PRODUCT_NAME := omni_m02s
 PRODUCT_BRAND := samsung
