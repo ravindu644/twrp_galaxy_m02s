b7534dd6b2349d3d026dc30c705d214fa58e6c35
= <=>
2024-07-20 10:34:38 +0530

    rem Android.mk

--- Android.mk
+++
@@ -1,7 +0,0 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-$(call inherit-product, vendor/twrp/Android.mk)
--- omni_m02s.mk
+++ omni_m02s.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
-
+$(call inherit-product, vendor/twrp/Android.mk)
 # Inherit from m02s device
 $(call inherit-product, device/samsung/m02s/device.mk)
 
