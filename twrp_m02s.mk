1738296d9cf0b7eea89ccf18710b462c638a60dc
= <=>
2024-07-21 12:38:40 +0530

    red-blue color inv fixed

--- twrp_m02s.mk
+++ twrp_m02s.mk
@@ -1,16 +1,22 @@
+
+PRODUCT_RELEASE_NAME := m02s
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
+
+#vendor/pb/config/common.mk for PBRP
+
 $(call inherit-product, vendor/twrp/Android.mk)
+
 # Inherit from m02s device
 $(call inherit-product, device/samsung/m02s/device.mk)
 
-#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/m02s/recovery/root,recovery/root)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/m02s/recovery/root/vendor/firmware,recovery/root/vendor/firmware)
 
-#Other inherits (not needed ig)
-#$(call inherit-product, vendor/twrp/config/packages.mk)
+#Other extra stuff
+$(call inherit-product, vendor/twrp/config/packages.mk)
 
 PRODUCT_DEVICE := m02s
 PRODUCT_NAME := twrp_m02s
